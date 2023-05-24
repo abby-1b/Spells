15e879175b0ec5f308d0dcf11f2c29576078a1b7
Code (I Guess) <46802090+CodeIGuess@users.noreply.github.com>
2023-05-23 22:39:57 -0400

    Added edge cases
    I need to work on a better system, as adding an extra condition would have this get out of hand.

--- install.ts
+++ install.ts
@@ -3,9 +3,30 @@ const cwd = new URL('.', import.meta.url).pathname
 
 const file = `deno run -A ${cwd}src/spells.ts "$@"`
 
+const location = "/usr/local/bin/spl"
+
 try {
-	Deno.writeTextFileSync("/usr/local/bin/spl", file)
-	Deno.run({ cmd: ["chmod", "+x", "/usr/local/bin/spl"] })
+	try {
+		// Try to install
+		await Deno.writeTextFile(location, file)
+	} catch {
+		// Apparently, sometimes `/usr/local/bin` doesn't exist???
+		// So make it.
+		await Deno.mkdir("/usr/local/bin")
+
+		// Then try to install.
+		await Deno.writeTextFile(location, file)
+	}
+
+	// await Deno.run({ cmd: ["chmod", "+x", "/usr/local/bin/spl"] }).status()
+	await new Deno.Command(Deno.execPath(), {
+		args: ["chmod", "+x", "/usr/local/bin/spl"]
+	}).output()
+
 } catch {
-	console.log("Please run the installer with sudo.")
+	// We're just gonna assume that the user didn't use `sudo`.
+	console.log("Please run this command with sudo.")
 }
+
+// TODO: install the VSCode extension automatically.
+// (or maybe just add it to the extension shop)
