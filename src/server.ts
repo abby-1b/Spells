ab9bc1a9c462b4fa054ebc237aa717a0dcaee88c
Abby.041b <46802090+abby-1b@users.noreply.github.com>
2023-07-20 17:10:13 -0400

    Added routing!
    Note that this is a breaking change (as evidenced by the change from 0.8 to 1.0) because the server options interface was changed from `StartServerOptions` to `ServerOptions`.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { extname } from "https://deno.land/std@0.165.0/path/mod.ts"
 import { contentType } from "https://deno.land/std@0.177.0/media_types/mod.ts"
 import { compile, compileTS, startTSServer } from "./compile.ts"
 
+const FAVICON = 'AAABAAEAEB%4 EAIABoBAAAFgAAACg%4 Q%4 I%5 EAI%9 Q%1p gDZgKIA1YBg%11 /2y+z/9svv//bL7//2y+//9svv//bL7//2y+//9svv//bL7//2y+//9svv//bL7//2y+//9svv//bL7//2y+x/9svv+ANWAY%a D/bL7//22/r/9svv//bL7//2y+//9svv//bb+//2y+/w%a gDZfOP9svv//bL7H/2y+/w%4 D/bL7//2/APw%a /2y+//9svv8%a P9sv3f/bL7/%5 P9svv//bL6v%5 P9svv+ANl84/2y+/w%a /2y+//9wvxj/cL8g/2y+/w%a /2y+/w%4 D/bL7/%a D/bL+f/2y+//9svsc%5 /22+7/9svvc%a P9svv//bL7f%5 P9svuf/bL7//22/bw%f P9svv//bL7/%5 P9svv8%l /2y+/w%4 D/bL7//2y+/w%k D/b79//2y+//9svv//bL7//26/Zw%a /26/b/9svv//bL7//2y+/4A3X28%q P9svv+AOGAQ/2y+//9svv//bL7//2y+//9svv//bL7/%5 P9svv8%v D/bb7v/2y+//9svv8%l /2y+//9svv//bb7H%11 P9svv//bL7//2y+xw%a /2y+1/9svv//bL+f%1c /2y+//9svv//bL7//2y+//9svuf/bL7/%1h P9svY//bL7/%a D/bL7//2y/fw%1m /2y+//9tvt//bL7v/2y+/w%1w D/bL7//2y+/w%3e //8%7 BwDgAALnQAAK21AACJkwAAy9MAAOPHAADoFwAA48cAAPGPAAD4HwAA+b8AAPw/AAD+fwAA//8AAA=='.replace(/%.*? /g, e => "A".repeat(parseInt(e.slice(1, -1), 36)))
 
 /** Text to be served, which overrides file access. */
 export const serveText: {
@@ -29,11 +30,11 @@ async function getData(f: string): Promise<Deno.FileInfo | undefined> {
 
 /** Handles a single request, which only uses the given path. */
 async function handleRequest(path: string, fullPath: string, silent?: boolean): Promise<Response> {
-	let throw404 = false
+	// No more favicon
+	if (path == "favicon.ico")
+		return new Response("You don't get a favicon.", { status: 404 })
 
-	// if (path.length == 0) {
-	// 	path = "/"
-	// }
+	let throw404 = false
 
 	// Default to `index.spl`, then check `index.html` and finally send a directory.
 	if (path.endsWith("/") || path.length == 0) {
@@ -111,36 +112,103 @@ async function handleRequest(path: string, fullPath: string, silent?: boolean): 
 }
 
 /** Handles a single connection to the server */
-async function handleConnection(conn: Deno.Conn, silent?: boolean) {
+async function handleConnection(conn: Deno.Conn, opts: ServerOptions) {
 	const httpConn = Deno.serveHttp(conn)
 	for await (const requestEvent of httpConn) {
+		// Craft the SpellsRequest object
 		const { request } = requestEvent
-		const url = new URL(request.url)
-		const path = url.pathname.substring(1)
-		requestEvent.respondWith(handleRequest(path, request.url, silent))
+			, url = new URL(request.url)
+			, path = url.pathname.substring(1)
+			, args: Record<string, string> = {}
+		for (const [key, value] of url.searchParams.entries())
+			args[key] = value
+		const splReq: SpellsRequest = {
+			url: request.url, args,
+			path: url.pathname.substring(1)
+		}
+
+		// Check through the routes...
+		let tookRoute = false
+		for (const r of opts.routes ?? []) {
+			if (!r.capture(splReq)) continue
+			// A route was found! Send its output to the client
+			requestEvent.respondWith(r.routingFn(splReq, opts))
+			tookRoute = true
+			break
+		}
+		if (tookRoute) continue
+
+		// If no suitable route was found, use the default function
+		requestEvent.respondWith(handleRequest(path, request.url, opts.silent))
 	}
 }
 
-interface StartServerOptions {
+interface ServerOptions {
 	/** The port number of the server */
-	port: number
+	readonly port: number
 
 	/** The address of the server. Defaults to "localhost" */
-	bind?: string
+	readonly bind?: string
 
 	/** Wether or not the server emits any messages */
-	silent?: boolean
+	readonly silent?: boolean
+
+	/** The routing functions used whenever a request is received */
+	readonly routes?: Route[]
+}
+
+interface Route {
+	/** If this function returns true, the output is routed through this route. */
+	capture: (req: SpellsRequest) => boolean,
+
+	/** If `capture` returns true, this function will be called. */
+	routingFn: (req: SpellsRequest, opts: ServerOptions) => Response
+}
+
+/** Creates a simple route that matches a path string. */
+function SimpleRoute(
+	path: string,
+	routingFn: (req: SpellsRequest, opts: ServerOptions) => Response
+): Route {
+	return {
+		capture: (req: SpellsRequest) => req.path == path,
+		routingFn
+	}
+}
+
+interface SpellsRequest {
+	/** The URL, just as it was passed to the server. */
+	url: string
+
+	/** The arguments passed through the URL arranged a dictionary */
+	args: Record<string, string>
+
+	/**
+	 * The path, without any of the hostname nor arguments.
+	 * Also note that the path doesn't start with a "/"
+	 */
+	path: string
 }
 
 /** Start the server (non-blocking) */
-export async function startServer(options: StartServerOptions) {
+export async function startServer(options: ServerOptions) {
 	startTSServer()
 	const listener = Deno.listen({ port: options.port })
 	if (!options.silent) console.log(`Server running at http://${options.bind ?? "localhost"}:${options.port}/`)
 	for await (const conn of listener)
-		handleConnection(conn, options.silent)
+		handleConnection(conn, options)
 }
 
 // Start if this isn't an import
-if (import.meta.main)
-	startServer({ port: 8080 })
+if (import.meta.main) {
+	startServer({ port: 8081, routes: [
+		SimpleRoute("someApi", req => {
+			const headers = new Headers()
+			headers.set("Content-Type", "text/json")
+			return new Response(
+				`{"working":true,"body":"It appears that everything is working!"}`,
+				{ headers }
+			)
+		})
+	] })
+}
